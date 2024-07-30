b548ee3c4fc1df3287890691b7e6721e4a9b17ea
Mwakai <mwakaimwambala@gmail.com>
2024-07-30 11:18:58 +0300

    feat:install vue

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,27 @@
 <?php
 
-use App\Http\Controllers\CaloriesController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-// Display the form to get nutrition information
-Route::get('/calories', [CaloriesController::class, 'showForm']);
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Handle the form submission and display nutrition information
-Route::get('/nutrition', [CaloriesController::class, 'getNutrition']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
+require __DIR__.'/auth.php';
